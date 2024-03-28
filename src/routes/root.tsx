58af75aa6dcc59d5c0250b4e41ba1eb66d095083
y28lyn <i.beirade@gmail.com>
2024-03-28 12:07:28 +0100

    fix: :bug: Mini fix

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -13,7 +13,6 @@ const Root = () => {
         rel="stylesheet"
         href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css"
       />
-
       <div className="bg-white dark:bg-[#101314]">
         <Navbar />
         <ScrollRestoration />
