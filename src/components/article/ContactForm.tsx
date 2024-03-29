5bc5763dff418dc260897285347a9eb5081f4683
y28lyn <i.beirade@gmail.com>
2024-03-29 14:19:08 +0100

    fix: :bug: EmailJS bug fixed

--- src/components/article/ContactForm.tsx
+++ src/components/article/ContactForm.tsx
@@ -1,5 +1,5 @@
 import emailjs from "emailjs-com";
-import React, { useState, ChangeEvent, FormEvent } from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
 
 interface ContactFormProps {
   title: string;
@@ -10,6 +10,11 @@ interface ContactFormProps {
 interface Errors {
   [key: string]: string;
 }
+
+const validatePhoneNumber = (phoneNumber: string): boolean => {
+  const phoneRegex = /^(0|\+33)[1-9]([-. ]?[0-9]{2}){4}$/;
+  return phoneRegex.test(phoneNumber);
+};
 
 const ContactForm: React.FC<ContactFormProps> = ({ title, subtitle, desc }) => {
   const [formData, setFormData] = useState({
@@ -26,7 +31,11 @@ const ContactForm: React.FC<ContactFormProps> = ({ title, subtitle, desc }) => {
     tempErrors.email = /^\S+@\S+\.\S+$/.test(formData.email)
       ? ""
       : "Adresse e-mail non valide.";
-    tempErrors.phone = formData.phone ? "" : "Ce champ est requis.";
+    tempErrors.phone = formData.phone
+      ? validatePhoneNumber(formData.phone)
+        ? ""
+        : "Numéro de téléphone non valide."
+      : "Ce champ est requis.";
     tempErrors.message = formData.message ? "" : "Ce champ est requis.";
     setErrors(tempErrors);
     return Object.keys(tempErrors).every((key) => !tempErrors[key]);
@@ -36,15 +45,25 @@ const ContactForm: React.FC<ContactFormProps> = ({ title, subtitle, desc }) => {
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
+
+  const [feedback, setFeedback] = useState({ message: "", error: false });
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!validateForm()) return;
 
     emailjs
-      .send("your_service_id", "your_template_id", formData, "your_user_id")
+      .send(
+        "service_paok9fw",
+        "template_ko7gg8c",
+        formData,
+        "vSzcB0UaQBXi-CJyl"
+      )
       .then(
         (result) => {
           console.log(result.text);
@@ -54,15 +73,23 @@ const ContactForm: React.FC<ContactFormProps> = ({ title, subtitle, desc }) => {
             phone: "",
             message: "",
           });
+          setFeedback({
+            message: "Votre message a été envoyé avec succès!",
+            error: false,
+          });
         },
         (error) => {
           console.log(error.text);
+          setFeedback({
+            message: "Une erreur s'est produite lors de l'envoi de l'e-mail.",
+            error: true,
+          });
         }
       );
   };
 
   return (
-    <section className="relative z-10 px-6 md:px-20 py-6 flex items-center justify-center">
+    <section className="relative z-10 px-10 md:px-20 py-6 flex items-center justify-center">
       <div className="container">
         <div className="flex flex-wrap lg:justify-between -mx-4">
           <div className="w-full lg:w-1/2 xl:w-6/12 px-4">
@@ -87,7 +114,7 @@ const ContactForm: React.FC<ContactFormProps> = ({ title, subtitle, desc }) => {
                     width="1em"
                     height="1.1em"
                     viewBox="0 0 512 512"
-                    stroke-width="0"
+                    strokeWidth="0"
                     fill="currentColor"
                     stroke="currentColor"
                     className="w-5 h-5"
@@ -110,8 +137,8 @@ const ContactForm: React.FC<ContactFormProps> = ({ title, subtitle, desc }) => {
                     className="w-5"
                   >
                     <path
-                      clip-rule="evenodd"
-                      fill-rule="evenodd"
+                      clipRule="evenodd"
+                      fillRule="evenodd"
                       fill="currentColor"
                       d="M7.49933 0.25C3.49635 0.25 0.25 3.49593 0.25 7.50024C0.25 10.703 2.32715 13.4206 5.2081 14.3797C5.57084 14.446 5.70302 14.2222 5.70302 14.0299C5.70302 13.8576 5.69679 13.4019 5.69323 12.797C3.67661 13.235 3.25112 11.825 3.25112 11.825C2.92132 10.9874 2.44599 10.7644 2.44599 10.7644C1.78773 10.3149 2.49584 10.3238 2.49584 10.3238C3.22353 10.375 3.60629 11.0711 3.60629 11.0711C4.25298 12.1788 5.30335 11.8588 5.71638 11.6732C5.78225 11.205 5.96962 10.8854 6.17658 10.7043C4.56675 10.5209 2.87415 9.89918 2.87415 7.12104C2.87415 6.32925 3.15677 5.68257 3.62053 5.17563C3.54576 4.99226 3.29697 4.25521 3.69174 3.25691C3.69174 3.25691 4.30015 3.06196 5.68522 3.99973C6.26337 3.83906 6.8838 3.75895 7.50022 3.75583C8.1162 3.75895 8.73619 3.83906 9.31523 3.99973C10.6994 3.06196 11.3069 3.25691 11.3069 3.25691C11.7026 4.25521 11.4538 4.99226 11.3795 5.17563C11.8441 5.68257 12.1245 6.32925 12.1245 7.12104C12.1245 9.9063 10.4292 10.5192 8.81452 10.6985C9.07444 10.9224 9.30633 11.3648 9.30633 12.0413C9.30633 13.0102 9.29742 13.7922 9.29742 14.0299C9.29742 14.2239 9.42828 14.4496 9.79591 14.3788C12.6746 13.4179 14.75 10.7025 14.75 7.50024C14.75 3.49593 11.5036 0.25 7.49933 0.25Z"
                     ></path>
@@ -129,6 +156,7 @@ const ContactForm: React.FC<ContactFormProps> = ({ title, subtitle, desc }) => {
                 <div className="mb-6">
                   <input
                     type="text"
+                    name="name"
                     placeholder="Votre nom et prénom"
                     value={formData.name}
                     onChange={handleChange}
@@ -139,6 +167,7 @@ const ContactForm: React.FC<ContactFormProps> = ({ title, subtitle, desc }) => {
                 <div className="mb-6">
                   <input
                     type="email"
+                    name="email"
                     placeholder="Votre e-mail"
                     value={formData.email}
                     onChange={handleChange}
@@ -151,6 +180,7 @@ const ContactForm: React.FC<ContactFormProps> = ({ title, subtitle, desc }) => {
                 <div className="mb-6">
                   <input
                     type="text"
+                    name="phone"
                     placeholder="Votre numéro de téléphone"
                     value={formData.phone}
                     onChange={handleChange}
@@ -163,6 +193,7 @@ const ContactForm: React.FC<ContactFormProps> = ({ title, subtitle, desc }) => {
                 <div className="mb-6">
                   <textarea
                     placeholder="Votre message"
+                    name="message"
                     value={formData.message}
                     onChange={handleChange}
                     className="w-full rounded py-3 px-[14px] text-body-color text-base border border-[f0f0f0] outline-none focus-visible:shadow-none focus:border-primary"
@@ -179,6 +210,15 @@ const ContactForm: React.FC<ContactFormProps> = ({ title, subtitle, desc }) => {
                     Envoyer
                   </button>
                 </div>
+                {feedback.message && (
+                  <p
+                    className={`text-sm mt-3 ${
+                      feedback.error ? "text-red-500" : "text-green-500"
+                    }`}
+                  >
+                    {feedback.message}
+                  </p>
+                )}
               </form>
             </div>
           </div>
